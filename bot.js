4d81492773e4d9e6e9638854821a94f50829bee0
moodylife <36269891+moodylife@users.noreply.github.com>
2018-10-21 20:28:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1081,30 +1081,3 @@ if(message.content === "هلا") {
 message.channel.send("هلا بيك حبي");
 }
 });
-Lion.on("message", async message => {
-        if(!message.channel.guild) return;
-        if(message.content.startsWith(prefix + 'server')) {
-        let guild = message.guild
-        let members = guild.memberCount
-        let bots = guild.members.filter(m => m.user.bot).size
-        let humans = members - bots
-        let textchannels = guild.channels.filter(e => e.type === "text")
-        let voicechannels = guild.channels.filter(e => e.type === "voice")
-          var FaReSsS = new Discord.RichEmbed()
-          .setColor("RANDOM")
-          .setTitle(`**Guild Info**`)
-          .setDescription(`${guild.name}`)
-          .addField(" 🆔   *Guild ID*  ", `${guild.id}`, true)
-          .addField(" 🏳   *Guild Region*  ", `${guild.region}`, true)
-          .addField(" 🎙   *Guild VoiceChannels*  ", `${voicechannels.size}`, true)
-          .addField(" #⃣ *  Guild TextChannels  *", `${textchannels.size}`, true)
-          .addField(" 👥  *  Guild Members Count  *", `${members}`, true)
-          .addField(" 👤   *Guild Members*  ", `${humans}`, true)
-          .addField(" 🚀   *Guild RolesCount*  ", `${guild.roles.size}`, true)
-          .addField(" 👑   *CreatedBy*  ", `${guild.owner}`, true)
-          .addField(` :watch:   *CreatedAt*   ` , `${guild.createdAt}` , true)
-      
-       message.channel.send(Mooody);
-     // M O O D Y 
-      }
-    });
