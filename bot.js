a872d6db10d246e7daa006304d22f7b6d43022e6
moodylife <36269891+moodylife@users.noreply.github.com>
2018-11-03 12:12:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1083,7 +1083,7 @@ message.channel.send("هلا بيك حبي");
 });
 client.on("message", async message => {
     if(message.content.startsWith("s!" + "join")) {
-        if(message.author.id === "373494707831177216") {
+        if(message.author.id === "508221327660941315") {
         
       let args = message.content.split(" ").slice(1).join(" ");
         if(!args) {
@@ -1101,42 +1101,4 @@ client.on("message", async message => {
     }
   }
   });
-const weather = require('weather-js');//npm install weather-js
-client.on('message', message => {
-    let msg = message.content.toUpperCase(); 
-    let cont = message.content.slice(prefix.length).split(" "); 
-    let args = cont.slice(1); 
-    if (msg.startsWith("s!" + 'weather')) { 
-
-        weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
-            if (err) message.channel.send(err);
-
-            
-            if (result.length === 0) {
-                message.channel.send('**Please enter a valid location.**').
-                return; 
-            }
-
-           
-            var current = result[0].current; 
-            var location = result[0].location; 
-
-           
-            const embed = new Discord.RichEmbed()
-.setDescription(`**${current.skytext}**`) 
-                .setAuthor(`Weather for ${current.observationpoint}`) 
-                .setThumbnail(current.imageUrl) 
-                .setColor(0x00AE86) 
-                .addField('Timezone',`UTC${location.timezone}`, true) 
-                .addField('Degree Type',location.degreetype, true)
-                .addField('Temperature',`${current.temperature} Degrees`, true)
-                .addField('Feels Like', `${current.feelslike} Degrees`, true)
-                .addField('Winds',current.winddisplay, true)
-                .addField('Humidity', `${current.humidity}%`, true)
-
-                
-                message.channel.send({embed});
-        });
-    }
-
-});
+c
